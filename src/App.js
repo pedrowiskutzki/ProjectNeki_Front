676407be202c440d207869049c2010c5da82dcdb
pedrowiskutzki <wiskutzki@gmail.com>
2023-01-05 00:03:35 -0300

    feat(Commit):first Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Pages/Login';
+import { GlobalStyle } from './Styles/global';
+
+export default function App() {
+ 
+  return (
+    <>
+    <GlobalStyle />
+    <Login/>
+    </>
+  );
+}
---
+++ src/Styles/global.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin:0px;
+    padding:0px;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 16px;
+   
+   
+    //Variáveis de cores
+    --cor-primaria:#19525E;  
+    --cor-secundaria: #8BE3F7;
+    --cor-complementar-1: #3EDEBE;
+    --cor-complementar-2: #454D4F;
+    --cor-complementar-3: #2D96AD;
+    --cor-complementar-4: #F7F7F7;
+
+  }
+  
+  input, button{
+    cursor: pointer;
+  }
+
+`;
