48133f3aaef23402984a8381dd0d2c15d74aaa2e
pedrowiskutzki <wiskutzki@gmail.com>
2023-01-06 16:58:30 -0300

    feat(Filter):Filter User Id

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -8,14 +8,17 @@ import Navbar from '../../Components/Navbar';
 import { SkillModal } from '../../Components/Modal';
 
 export default function Home() {
-  const [skill, setSkill] = useState([]);
+  const [userId, setUserId] = useState('');
   const [pessoaSkill, setPessoaSkill] = useState([]);
   const [modalIsOpen, setIsOpen] = useState(false);
-  const { userId } = useContext(AuthContext);
   const [removeLoading, setRemoveLoading] = useState(false);
   
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
   useEffect(() => {
+    get()
     pessoaSkillService
       .getAll()
       .then((res) => {
@@ -26,6 +29,23 @@ export default function Home() {
         console.error("ops! ocorreu um erro" + err);
       });
   }, []);
+
+  function handleDeleteEventoEspecial(id) {
+    pessoaSkillService
+      .remove(id)
+      .then((response) => {
+        alert("Skill Deletada")
+        refreshPage();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const get = async () => {
+    const id = await localStorage.getItem("@id");
+    setUserId(id)
+  }
 
   function openModal() {
     setIsOpen(true);
@@ -48,14 +68,15 @@ export default function Home() {
           ) : (
             pessoaSkill.map((userSkill) => (
               <>
-              {userId === userSkill.pessoa.id ?(
+              {userId != userSkill.pessoa.id ?(
                 <p></p>
               ) : (
-                <Card>
+                <Card id={userSkill.id}>
                   <img src={userSkill.skill.image_url} width="140px" height="140px" />
                   <h1>{userSkill.skill.name}</h1>
                   <p>{userSkill.skill.description}</p>
                   <h3>{userSkill.knowledge_level}</h3>
+                  <button onClick={() => handleDeleteEventoEspecial(userSkill.id)}>Remover</button>
                 </Card>
                 )}
               </>
